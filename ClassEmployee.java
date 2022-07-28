56e51ada9b4439c90c35642d227ff85e2342589e
Saadee15 <99344828+Saadee15@users.noreply.github.com>
2022-07-28 18:57:31 -0500

    Add files via upload

---
+++ ClassEmployee.java
@@ -0,0 +1,20 @@
+package Question1;
+
+public class ClassEmployee {
+
+	String Name;
+	int age;
+	
+	public ClassEmployee(String Name, int i) 
+	{
+		System.out.println(Name);
+		System.out.println(i);
+	}
+
+	
+	
+
+		
+	}
+
+
---
+++ EmployeeInfo.java
@@ -0,0 +1,10 @@
+package Question1;
+
+public class EmployeeInfo {
+
+	public static void main(String[] args) {
+		ClassEmployee dispobj =new ClassEmployee("SaadAsif", 1995);
+		
+	}
+
+}
