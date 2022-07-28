f5d2a952d0a9e7d0fd57bb751a6209fcf3d78105
Saadee15 <99344828+Saadee15@users.noreply.github.com>
2022-07-28 18:55:59 -0500

    Add files via upload

---
+++ CLassB.java
@@ -0,0 +1,17 @@
+package AccessModifiers1;
+
+public class CLassB {
+
+	public static void main(String[] args) {
+		
+		ClassA Obj = new ClassA();
+		Obj.Method1();
+		Obj.DefaultMethod();
+		Obj.MethodABC();
+		
+		
+		
+
+	}
+
+}
---
+++ ClassA.java
@@ -0,0 +1,19 @@
+package AccessModifiers1;
+
+public class ClassA {
+	
+	public void Method1()
+	{
+		System.out.println("Method1");
+	}
+	static void DefaultMethod()
+	{
+		System.out.println("DefaultMethod");
+	}
+	
+	protected void MethodABC()
+	{
+		System.out.println("Protected method");
+
+}
+}
