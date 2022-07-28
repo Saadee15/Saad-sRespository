2ef9a867e04488f876679fbe7707a8bd48be36fe
Saadee15 <99344828+Saadee15@users.noreply.github.com>
2022-07-28 18:58:00 -0500

    Add files via upload

---
+++ StudentClass.java
@@ -0,0 +1,11 @@
+package Question2;
+
+public class StudentClass {
+
+	public static void main(String[] args) {
+		StudentInformation DispObj=new StudentInformation("Saad Asif",25);
+		
+
+	}
+
+}
---
+++ StudentInformation.java
@@ -0,0 +1,16 @@
+package Question2;
+
+public class StudentInformation {
+
+	int age = 25;
+	String StudentName;
+	
+	public StudentInformation (String StudentName,int age)
+	{
+		System.out.println(StudentName);
+		System.out.println(age);
+	}
+	
+
+	
+}
